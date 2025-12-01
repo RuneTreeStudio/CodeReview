ff427cb2e3a7bfb3f98e86dc361c76a4484616ea
RuneTreeStudio <128747281+RuneTreeStudio@users.noreply.github.com>
2025-12-01 09:57:11 +0100

    Add defult tests for class

---
+++ src/test/java/AwesomePasswordCheckerTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+public class AwesomePasswordCheckerTest {
+
+    @org.junit.Test
+    public void getInstance() {
+    }
+
+    @org.junit.Test
+    public void testGetInstance() {
+    }
+
+    @org.junit.Test
+    public void maskAff() {
+    }
+
+    @org.junit.Test
+    public void getDistance() {
+    }
+
+    @org.junit.Test
+    public void computeMd5() {
+    }
+}
